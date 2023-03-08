f36696cccbacd5938dff3a8ba1d59043893dbbaf
Blindtroop <101224575+Blindtroop@users.noreply.github.com>
2023-03-08 22:21:12 +0300

    Added buttons

---
+++ script.js
@@ -0,0 +1,13 @@
+
+/*var navLinks=document.getElementbyId("navLinks");
+function showMenu(){
+    navLinks.style.right="0";
+}
+function hideMenu(){
+    navLinks.style.right="-200px";
+}
+*/
+
+const navOpenButton=document.querySelector("#nav-open");
+const navCloseButton=document.querySelector("#nav-close");
+
